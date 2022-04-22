dd9a522a5dd57568042901755d3ebf5a62286781
Joachim Rorbol <jr@efio.dk>
2022-04-22 11:40:22 +0200

    Initial commit

---
+++ src/InputValidation/Validator.cs
@@ -0,0 +1,10 @@
+﻿namespace InputValidation;
+
+public class Validator : IValidator
+{
+  public bool IsValidEmail(string email)
+  {
+    // TODO - replace the below exception with your own implementation
+    throw new NotImplementedException();
+  }
+}
